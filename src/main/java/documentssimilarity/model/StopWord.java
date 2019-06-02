e6e0b5fcda85f016f6a239e810789386968c3334
Guilherme Garcia <guilhermegs@ciandt.com>
2019-06-02 13:30:25 -0300

    Process - stop words

--- src/main/java/documentssimilarity/model/StopWord.java
+++ src/main/java/documentssimilarity/model/StopWord.java
@@ -8,11 +8,29 @@ public class StopWord {
 	List<String> words;
 
 	public static List<String> getStopWords() {
-		return Arrays.asList("a", "e", "i", "o", "u",
+		return Arrays.asList(
+				// artigos
+				"o", "a", "os", "as",
+				"um", "uma", "uns", "umas",
+
+				// artigos + preposições
+				"ao", "à", "aos", "às",
+				"do","da","dos","das",
+				"no", "na", "nos", "nas",
+				"pelo", "pela", "pelos", "pelas",
+				"num","numa","nuns","numas",
+				"dum","duma","duns","dumas",
+
+				// preposições
 				"ante", "até", "após", "com", "contra", "de",
 				"desde", "em", "entre", "para", "por", "perante",
 				"ser", "sem", "sob", "sobre", "traz",
-				" ", "  ");
+
+				// palavras descobertas conforme execução do processamento
+				" ", "Capítulo", "capítulo",
+				"httpsitesgooglecomsitebiblialivre",
+				"1","2","3","4","5","6","7","8","9","10",
+				"14","2012");
 	}
 
 }
